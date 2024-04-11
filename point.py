75f5fa83627bed4fc10c481ea4965b984db0610e
ilyadev750 <petrovich76007600kz@gmail.com>
2024-04-11 14:06:32 +0200

    test task is done

--- point.py
+++ point.py
@@ -25,12 +25,12 @@ class Point:
         self.__coord_y = y
 
     def get_coord(self):
-        return self.__coord_x, self.__coord_y
+        return self.coord_x, self.coord_y
 
     def set_coord(self, x, y):
-        self.__coord_x = x
-        self.__coord_y = y
+        self.coord_x = x
+        self.coord_y = y
 
     def distance_to_the_another_point(self, point_x, point_y):
-        self.distance = math.sqrt((self.__coord_x - point_x)**2
-                                  + (self.__coord_y - point_y)**2)
+        self.distance = math.sqrt((self.coord_x - point_x)**2
+                                  + (self.coord_y - point_y)**2)
